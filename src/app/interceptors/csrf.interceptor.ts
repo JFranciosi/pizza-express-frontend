adf53895d832f7c2481e5391705488463d954303
JFranciosi <luigigigio.jf@gmail.com>
2026-01-03 20:32:27 +0100

    feat: Add CSRF interceptor to automatically include XSRF token from cookie in request headers.

--- src/app/interceptors/csrf.interceptor.ts
+++ src/app/interceptors/csrf.interceptor.ts
@@ -2,28 +2,21 @@ import { HttpInterceptorFn } from '@angular/common/http';
 import { tap } from 'rxjs/operators';
 
 const CSRF_HEADER_NAME = 'X-XSRF-TOKEN';
-const CSRF_STORAGE_KEY = 'XSRF-TOKEN';
+const CSRF_COOKIE_NAME = 'XSRF-TOKEN';
 
 export const csrfInterceptor: HttpInterceptorFn = (req, next) => {
     let requestToForward = req;
-    const storedToken = localStorage.getItem(CSRF_STORAGE_KEY);
 
-    if (storedToken && !['GET', 'HEAD', 'OPTIONS'].includes(req.method)) {
+    // Read token from cookie
+    const token = document.cookie.split('; ')
+        .find(row => row.startsWith(CSRF_COOKIE_NAME + '='))
+        ?.split('=')[1];
+
+    if (token && !['GET', 'HEAD', 'OPTIONS'].includes(req.method)) {
         requestToForward = req.clone({
-            headers: req.headers.set(CSRF_HEADER_NAME, storedToken)
+            headers: req.headers.set(CSRF_HEADER_NAME, decodeURIComponent(token))
         });
     }
 
-    return next(requestToForward).pipe(
-        tap({
-            next: (event) => {
-                if (event.type === 4) {
-                    const tokenFromHeader = event.headers.get(CSRF_HEADER_NAME);
-                    if (tokenFromHeader) {
-                        localStorage.setItem(CSRF_STORAGE_KEY, tokenFromHeader);
-                    }
-                }
-            }
-        })
-    );
+    return next(requestToForward);
 };
